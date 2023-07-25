6e7fec6e28b6d6fb7a101c0c3623906b6668ecf4
Chen-Yulin <chenyulin@sjtu.edu.cn>
2023-07-25 21:47:28 +0800

    :memo: finish optimization 1

--- final_project/new-forward.cuh
+++ final_project/new-forward.cuh
@@ -13,41 +13,44 @@ namespace op
 
 __global__ void forward_kernel(float *y, const float *x, const float *k, const int B, const int M, const int C, const int H, const int W, const int K)
 {
+// some kind macro definition
 #define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
 #define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
 #define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]	
-    
 
     const int H_out = H - K + 1;
     const int W_out = W - K + 1;
 
-    int W_grid = ceil(1.0*W_out/16.0);
+    int W_grid = ceil(1.0*W_out / TILE_WIDTH);
+    // the width of the input shared image
     int X_out_width = TILE_WIDTH + K -1;
 
+    // shared memory
     extern __shared__ float shared_mem[];
-
+    // assign start pointer
     float* X_shared=&shared_mem[0];
     float* W_shared=&shared_mem[X_out_width * X_out_width];
+
 
     int n = blockIdx.x; 
     int m=blockIdx.y; 
     int w0=threadIdx.x;
     int h0=threadIdx.y;
-
-    int h_base=(blockIdx.z/W_grid)*TILE_WIDTH; 
-    int w_base=(blockIdx.z % W_grid)*TILE_WIDTH; 
-
+    int h_base=(blockIdx.z / W_grid) * TILE_WIDTH; 
     int h=h_base+h0; 
+    int w_base=(blockIdx.z % W_grid) * TILE_WIDTH; 
     int w=w_base+w0;
 
-    float acc=0;
-
+    float res=0;
     for (int c=0; c<C; c++){
+
+        // load kernel in shared mem
 	    if ((h0<K) && (w0<K)){
-		W_shared[h0*K+w0]=k4d(m,c,h0,w0);
+		    W_shared[h0*K+w0]=k4d(m,c,h0,w0);
 	    }
 	    __syncthreads();
-	    
+
+	    // load input in shared men
 	    for (int i=h; i<h_base+X_out_width; i+=TILE_WIDTH){
 		    for (int j=w; j<w_base+X_out_width; j+=TILE_WIDTH){
 			    if (i<H && j<W){
@@ -59,19 +62,23 @@ __global__ void forward_kernel(float *y, const float *x, const float *k, const i
 		    }
 	    }
 	    __syncthreads();
+
+        // get partial product
 	    for (int p=0; p<K; p++){
 		    for (int q=0; q<K; q++){
 			    if(((h0+p) < X_out_width) && ((w0+q) < X_out_width)){
-				    acc+=X_shared[(h0+p)*(X_out_width) + (w0+q)] * W_shared[p*K+q];
+				    res+=X_shared[(h0+p)*(X_out_width) + (w0+q)] * W_shared[p*K+q];
 			    }
 		    }
 	    }
 	    __syncthreads();
     }
+    // write result
     if (n<B && m<M && h<H_out && w<W_out){
-	    y4d(n,m,h,w)=acc;
+	    y4d(n,m,h,w)=res;
     }
 
+// end definition
 #undef y4d
 #undef x4d
 #undef k4d
@@ -97,21 +104,17 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tenso
     const int W = x.shape_[3];
     const int K = w.shape_[3];
 
+
     const int H_out = H - K + 1;
     const int W_out = W - K + 1;
-
     const int W_grid = ceil(1.0*W_out/TILE_WIDTH);
     const int H_grid = ceil(1.0*H_out/TILE_WIDTH);
     const int Z = W_grid * H_grid;
-
     dim3 blockDim(TILE_WIDTH, TILE_WIDTH, 1);
     dim3 gridDim(B, M, Z);
     
-    //cudaMemcpyToSymbol(mc, w.dptr_, sizeof(float) * M * C * K * K);
-
     // Call the kernel
     forward_kernel<<<gridDim, blockDim, sizeof(float)*((TILE_WIDTH+K-1)*(TILE_WIDTH+K-1)+K * K)>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
-
 
     // Use MSHADOW_CUDA_CALL to check for CUDA runtime errors.
     MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
