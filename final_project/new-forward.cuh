6f5ae691ba6475f5fe5f99fc291243be8b28f5e4
Chen-Yulin <chenyulin@sjtu.edu.cn>
2023-07-25 21:48:47 +0800

    :memo: finish optimization 2

--- final_project/new-forward.cuh
+++ final_project/new-forward.cuh
@@ -1,6 +1,7 @@
-
 #ifndef MXNET_OPERATOR_NEW_FORWARD_CUH_
 #define MXNET_OPERATOR_NEW_FORWARD_CUH_
+
+#define TILE_WIDTH 16
 
 #include <mxnet/base.h>
 
@@ -8,64 +9,76 @@ namespace mxnet
 {
 namespace op
 {
+__constant__ float w_const[12000] 
 
 __global__ void forward_kernel(float *y, const float *x, const float *k, const int B, const int M, const int C, const int H, const int W, const int K)
 {
-
-    /*
-    Modify this function to implement the forward pass described in Chapter 16.
-    We have added an additional dimension to the tensors to support an entire mini-batch
-    The goal here is to be correct AND fast.
-    We have some nice #defs for you below to simplify indexing. Feel free to use them, or create your own.
-    */
+// some kind macro definition
+#define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
+#define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
+#define k4d(i3, i2, i1, i0) w_const[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]	
 
     const int H_out = H - K + 1;
     const int W_out = W - K + 1;
 
-    // 计算线程索引
-    int b = blockIdx.x;
-    int m = blockIdx.y;
-    int h = threadIdx.y;
-    int w = threadIdx.z;
+    int W_grid = ceil(1.0*W_out / TILE_WIDTH);
+    // the width of the input shared image
+    int X_out_width = TILE_WIDTH + K -1;
 
-    // 初始化输出
-    float sum = 0;
+    // shared memory
+    extern __shared__ float shared_mem[];
+    // assign start pointer
+    float* X_shared=&shared_mem[0];
+    float* W_shared=&shared_mem[X_out_width * X_out_width];
 
-    // 循环遍历输入特征图
-    for (int c = 0; c < C; c++)
-    {
-        // 计算卷积
-        for (int p = 0; p < K; p++)
-            for (int q = 0; q < K; q++)
-                sum += x4d(b, c, h + p, w + q) * const_k[c * K * K + p * K + q];
+
+    int n = blockIdx.x; 
+    int m=blockIdx.y; 
+    int w0=threadIdx.x;
+    int h0=threadIdx.y;
+    int h_base=(blockIdx.z / W_grid) * TILE_WIDTH; 
+    int h=h_base+h0; 
+    int w_base=(blockIdx.z % W_grid) * TILE_WIDTH; 
+    int w=w_base+w0;
+
+    float res=0;
+    for (int c=0; c<C; c++){
+
+        // load kernel in shared mem
+	    if ((h0<K) && (w0<K)){
+		    W_shared[h0*K+w0]=w_const
+	    }
+	    __syncthreads();
+
+	    // load input in shared men
+	    for (int i=h; i<h_base+X_out_width; i+=TILE_WIDTH){
+		    for (int j=w; j<w_base+X_out_width; j+=TILE_WIDTH){
+			    if (i<H && j<W){
+				    X_shared[(i-h_base)*(X_out_width)+(j-w_base)]=x4d(n,c,i,j);
+			    }
+			    else{
+				    X_shared[(i-h_base)*(X_out_width)+(j-w_base)]=0;
+			    }
+		    }
+	    }
+	    __syncthreads();
+
+        // get partial product
+	    for (int p=0; p<K; p++){
+		    for (int q=0; q<K; q++){
+			    if(((h0+p) < X_out_width) && ((w0+q) < X_out_width)){
+				    res+=X_shared[(h0+p)*(X_out_width) + (w0+q)] * k4d(m,c,p,q);
+			    }
+		    }
+	    }
+	    __syncthreads();
+    }
+    // write result
+    if (n<B && m<M && h<H_out && w<W_out){
+	    y4d(n,m,h,w)=res;
     }
 
-    // 将结果写入全局内存
-    y4d(b, m, h, w) = sum;
-
-// An example use of these macros:
-// float a = y4d(0,0,0,0)
-// y4d(0,0,0,0) = a
-#define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
-#define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
-#define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
-
-    int b = blockDim.x * blockIdx.x + threadIdx.x;
-
-    if (b < B) // for each image in the batch
-    {
-        for (int m = 0; m < M; m++)         // for each output feature maps
-            for (int h = 0; h < H_out; h++) // for each output element
-                for (int w = 0; w < W_out; w++)
-                {
-                    y4d(b, m, h, w) = 0;
-                    for (int c = 0; c < C; c++)     // sum over all input feature maps
-                        for (int p = 0; p < K; p++) // KxK filter
-                            for (int q = 0; q < K; q++)
-                                y4d(b, m, h, w) += x4d(b, c, h + p, w + q) * k4d(m, c, p, q);
-                }
-    }
-
+// end definition
 #undef y4d
 #undef x4d
 #undef k4d
@@ -85,20 +98,26 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tenso
     // CHECK_EQ(0, 1) << "Remove this line and replace with your implementation";
 
     const int B = x.shape_[0];
-    const int M = y.shape_[1]; // num_filter
+    const int M = y.shape_[1];
     const int C = x.shape_[1];
     const int H = x.shape_[2];
     const int W = x.shape_[3];
     const int K = w.shape_[3];
 
-    // 将卷积核复制到常量内存中
-    cudaMemcpyToSymbol(const_k, k, sizeof(float) * C * K * K);
 
-    dim3 gridDim((B + 511) / 512);
-    dim3 blockDim(512);
+    const int H_out = H - K + 1;
+    const int W_out = W - K + 1;
+    const int W_grid = ceil(1.0*W_out/TILE_WIDTH);
+    const int H_grid = ceil(1.0*H_out/TILE_WIDTH);
+    const int Z = W_grid * H_grid;
+    dim3 blockDim(TILE_WIDTH, TILE_WIDTH, 1);
+    dim3 gridDim(B, M, Z);
+    
+    cudaMemcpyToSymbol(w_const, w.dptr_, sizeof(float) * M * C * K * K);
+    // Call the kernel
+    forward_kernel<<<gridDim, blockDim, sizeof(float)*((TILE_WIDTH+K-1)*(TILE_WIDTH+K-1)+K * K)>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
 
-    MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
-    forward_kernel<<<gridDim, blockDim>>>(y.dptr_, x.dptr_, w.dptr_, B, M, C, H, W, K);
+    // Use MSHADOW_CUDA_CALL to check for CUDA runtime errors.
     MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
 }
 
@@ -115,3 +134,4 @@ void forward(mshadow::Tensor<gpu, 4, DType> &y, const mshadow::Tensor<gpu, 4, DT
 }
 
 #endif
+
